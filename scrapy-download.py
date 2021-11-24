ffed8da96c008fb5fe635f03914f5454172ff417
Guilherme Fernandes <57098649+guilfer-dev@users.noreply.github.com>
2021-11-24 00:06:30 -0300

    Add folder for downloads & reformat comments (auto)

--- scrapy-download.py
+++ scrapy-download.py
@@ -1,29 +1,36 @@
 # Created By  : Anderval
 # Created Date: 20/11/2021
 
-import requests #Importa a Biblioteca requests
-from bs4 import BeautifulSoup #Importa a Biblioteca BeautifulSoup
+import requests  # Importa a Biblioteca requests
+from bs4 import BeautifulSoup  # Importa a Biblioteca BeautifulSoup
 
 print("Starting scrap...")
-for x in range(9): #loop que irá pecorrer o número de temporadas
-  for i in range(12): #loop que irá pecorrer o número de episódios
+for x in range(9):  # loop que irá pecorrer o número de temporadas
+    for i in range(12):  # loop que irá pecorrer o número de episódios
 
-    #Url utilizada para realizar o scrapy, a url recebe o numero da temporada e do episódio pelo loop
-    url = "http://www.ayoitsdistrict78.com/album/abdc-season-{}-episode-{}/".format(x,i) 
-    req = requests.get(url) #Envia uma resiquição get para a página
-    soup = BeautifulSoup(req.content, 'html.parser') #recebe o conteúdo da página verificando os marcadores html
+        # Url utilizada para realizar o scrapy, a url recebe o numero da temporada e do episódio pelo loop
+        url = "http://www.ayoitsdistrict78.com/album/abdc-season-{}-episode-{}/".format(
+            x, i)
+        req = requests.get(url)  # Envia uma resiquição get para a página
+        # recebe o conteúdo da página verificando os marcadores html
+        soup = BeautifulSoup(req.content, 'html.parser')
 
-    for link in soup.find_all("li"): #loop que pecorre todos elementos li, os elementos li é onde estão nossos arquivos mp3
-        linkMp3 = format(link.get("data-media")) #Armazenando o endereço que está na tag data-media
-        nomeArquivo = nomeArquivo = linkMp3.split("/", -1)[-1] #Pegando o nome do arquivo à partir do da ultima parta da url
+        # loop que pecorre todos elementos li, os elementos li é onde estão nossos arquivos mp3
+        for link in soup.find_all("li"):
+            # Armazenando o endereço que está na tag data-media
+            linkMp3 = format(link.get("data-media"))
+            # Pegando o nome do arquivo à partir do da ultima parta da url
+            nomeArquivo = nomeArquivo = linkMp3.split("/", -1)[-1]
 
-        if linkMp3.endswith(".mp3"): #Condicional que verifica se o endereço armazenado contem a extensão .mp3 
-            print("Realizando o download do arquivo: "+ nomeArquivo)
-            print(linkMp3)
+            # Condicional que verifica se o endereço armazenado contem a extensão .mp3
+            if linkMp3.endswith(".mp3"):
+                print("Realizando o download do arquivo: " + nomeArquivo)
+                print(linkMp3)
 
-            doc = requests.get(linkMp3) #Envia uma resiquição get para a página
-            with open(nomeArquivo, 'wb') as f:#Abrindo o arquivo passando nome, e habilitando a escrita do arquivo binário
-                f.write(doc.content) #Salvando o conteudo da requisição no arquivo
-                f.close() #Fecha arquivo após a conclusão
-
-    
+                # Envia uma resiquição get para a página
+                doc = requests.get(linkMp3)
+                # Abrindo o arquivo passando nome, e habilitando a escrita do arquivo binário
+                with open("./downloads/%s" % (nomeArquivo), 'wb') as f:
+                    # Salvando o conteudo da requisição no arquivo
+                    f.write(doc.content)
+                    f.close()  # Fecha arquivo após a conclusão
